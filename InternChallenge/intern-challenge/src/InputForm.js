4c1bf758806b5572873bb3be35d2d7419c6ed857
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-05-16 19:24:57 -0700

    getting responses, had to change the env variable to work with react;

--- InternChallenge/intern-challenge/src/InputForm.js
+++ InternChallenge/intern-challenge/src/InputForm.js
@@ -1,32 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 //this is also where I want to make the api call when they hit submit
 
 const InputForm = () => {
   const [poetryPrompt, setPoetryPrompt] = useState('');
+  const [response, setResponse] = useState('');
+
+  //const [poem, setPoem] = useState('');
+
+  // useEffect(() => {
+  //   if (response.length) {
+  //     setPoem(response.choices[0].text)
+  //     console.log(poem)
+  //   }
+  // }, [response, poem]);
+
+  const prompt = {
+    "prompt": `${poetryPrompt}`,
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    //also make call to api here but write it as a separate func I think
   };
+
+  const writePoem = (data) => {
+    console.log('clicked')
+    fetch('https://api.openai.com/v1/engines/text-curie-001/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        setResponse(json);
+      });
+    };
+
+  console.log(response.choices[0].text);
 
   const handleInputChange = (event) => {
     const poetryPrompt = event.target.value;
     setPoetryPrompt(poetryPrompt);
-    console.log(poetryPrompt)
   };
+
   return (
     <>
-    <form onSubmit={handleSubmit} id='poetry-prompt-container'>
-      <input id='poetry-prompt'
-        placeholder='Write a poem about...'
-        value={poetryPrompt}
-        onChange={handleInputChange}
-      />
-    </form>
-    <button>
-      Generate Poem
-    </button>
+      <form onSubmit={handleSubmit} id='poetry-prompt-container'>
+        <input
+          id='poetry-prompt'
+          placeholder='Write a poem about...'
+          value={poetryPrompt}
+          onChange={handleInputChange}
+        />
+      </form>
+      <button onClick={() => writePoem(prompt)}>Generate Poem</button>
     </>
   );
 };
